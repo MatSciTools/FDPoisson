eeb86ba066ac34c0d79548202056cde6fc0e4d61
Vishnu Raghuraman <vishnu.raman@gmail.com>
2021-05-12 23:25:03 -0400

    Added 2D capability

--- src/OutputHandler.F90
+++ src/OutputHandler.F90
@@ -1,15 +1,16 @@
 module OutputHandler
 
-public :: output1DSolution
+public :: output1DSolution, &
+          output2DSolution
 
 private
    integer, parameter :: int_val = kind(1)
    integer, parameter :: real_val = kind(1.0d0)
 
 contains
-!  --------------------------------------------------
+!  ------------------------------------------------------------
    subroutine output1DSolution(x, sol, Ex)
-!  --------------------------------------------------
+!  ------------------------------------------------------------
 
    real (kind=real_val), intent(in), pointer :: x(:)
    real (kind=real_val), intent(in), pointer :: sol(:)
@@ -30,5 +31,30 @@ contains
    close(2)
 
    end subroutine output1DSolution
-!  --------------------------------------------------
+!  -------------------------------------------------------------
+   subroutine output2DSolution(x, y, phi)
+!  -------------------------------------------------------------
+
+   real (kind=real_val), intent(in), pointer :: x(:)
+   real (kind=real_val), intent(in), pointer :: y(:)
+   real (kind=real_val), intent(in), pointer :: phi(:,:)
+   integer (kind=int_val) :: i, j, nx, ny
+   character (len=20) :: outfile   
+
+   nx = size(x)
+   ny = size(y)
+   outfile = "potential"
+   open(2, file=outfile, action='write')
+   write(2,*) "          x             ", &
+              "          y             ", &
+              "         Phi            "
+
+   do j = 1, ny
+     do i = 1, nx
+       write(2,*) x(i), y(j), phi(j, i)
+     enddo
+   enddo
+
+   end subroutine output2DSolution
+!  -------------------------------------------------------------
 end module OutputHandler
